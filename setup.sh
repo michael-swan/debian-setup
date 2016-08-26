bf915570378f3479aa1ac463d2042ae24849ed57
Michael Swan <mswan@fastmail.com>
2016-08-26 16:20:59 -0500

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# Setup environment
+die() { echo FAILURE: "$@" 1>&2; exit 1; }
+
+cd `dirname "$0"` \
+	|| die "Enter script's directory"
+
+# 0. Fix apt
+cat >/etc/apt/sources.list <<-EOF
+deb http://ftp.us.debian.org/debian/ jessie main contrib non-free
+deb http://ftp.us.debian.org/debian/ jessie-updates main contrib non-free
+deb http://security.debian.org/ jessie/updates main contrib non-free
+EOF	|| die 'Select repositories'
+
+install -o root -g root -m 644 misc/10no-check-valid-until /etc/apt/apt.conf.d/10no-check-valid-until \
+	|| die 'Fix repository "valid until" behaviour'
+
+dpkg --add-architecture i386 \
+	|| die 'Add i386 architecture'
+apt update \
+	|| die 'Package database update'
+apt dist-upgrade \
+	|| die 'Package upgrade'
+
+# 1. Install base packages
+apt install `cat pkgs.txt` \
+	|| die 'Install base packages'
+
+# 2. Configure default user
+user=`getent passwd 1000 | cut -d: -f1` \
+	|| die "Find default user's username"
+adduser "$user" sudo \
+	|| die 'Add default user to sudo group'
+
+# 3. Fix fonts
+rm /etc/fonts/conf.d/{10-scale-bitmap-fonts.conf,70-no-bitmaps.conf} \
+	|| die 'Enable bitmap fonts'
+sed -i 's/Bitstream Vera/Liberation/; s/Liberation Sans Mono/Liberation Mono/' /etc/fonts/conf.d/*-latin.conf \
+	|| die 'Configure latin fonts'
+install -o root -g root -m 644 misc/local.conf /etc/fonts/local.conf \
+	|| die 'Fix font aliasing and hinting'
+apt remove fonts-dejavu-core ttf-bitstream-vera fonts-droid \
+	|| die 'Uninstall awful fonts'
+
+# 4. Install Gohu and Tahoma fonts
+cp -rp gohu/ /usr/share/fonts/X11/ \
+	|| die 'Copy gohu/'
+cp -rp tahoma/ /usr/share/fonts/truetype/ \
+	|| die 'Copy tahoma/'
+chown -R root:root /usr/share/fonts/X11/gohu/ /usr/share/fonts/truetype/tahoma/ \
+	|| die 'Ensure proper ownership of gohu/ and tahoma/'
+
+# 5. Install Windows 2000 Theme
+cp -rp win2k/ /usr/share/themes/ \
+	|| die 'Copy win2k/'
+chown -R root:root /usr/share/themes/win2k/ \
+	|| die 'Ensure proper ownership of win2k/'
+
+# 6. Configure XTerm
+install -o root -g root -m 644 misc/xterm /etc/X11/Xresources/xterm \
+	|| die 'Configure XTerm'
+
+# 7. Fix key repeat
+install -o root -g root -m 644 misc/70-key-repeat /etc/X11/Xsession.d/70-key-repeat \
+	|| die 'Fix key repeat'
+
+# 8. Configure OpenBox
+cp -rp openbox/ /etc/xdg/ \
+	|| die 'Copy openbox/'
+chown -R root:root /etc/xdg/openbox/ \
+	|| die 'Ensure proper ownership of openbox/'
+
+# 9. Configure Tint2
+install -o root -g root -m 644 misc/tint2rc /etc/xdg/tint2/tint2rc \
+	|| die 'Configure Tint2'
